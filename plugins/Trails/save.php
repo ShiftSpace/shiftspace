84c664d14ed19b638a129207e1da74c9fcc68979
david <david>
2008-05-22 03:56:31 +0000

    modified trail load to join against user table.

--- plugins/Trails/save.php
+++ plugins/Trails/save.php
@@ -1,14 +1,18 @@
 <?php 
 
-if (empty($user) || empty($user->id)) {
-    echo "{status: 0, message:'User not logged in'}";
-    exit;
+if (empty($user) || empty($user->id)) 
+{
+  echo "{status: 0, message:'User not logged in'}";
+  exit;
 }
 
-if (!empty($_POST['href'])) {
-    $href = $db->escape($_POST['href']);
-} else if (!empty($_SERVER['HTTP_REFERER'])) {
-    $href = $db->escape($_SERVER['HTTP_REFERER']);
+if (!empty($_POST['href'])) 
+{
+  $href = $db->escape($_POST['href']);
+} 
+else if (!empty($_SERVER['HTTP_REFERER'])) 
+{
+  $href = $db->escape($_SERVER['HTTP_REFERER']);
 }
 
 $trailId = $db->escape($_POST['trailId']);
@@ -19,60 +23,62 @@ if($trailId)
 {
   // If it does update it
   $exists = $db->value("
-      SELECT id
-      FROM trail
-      WHERE id = '$trailId'
-  ");
+    SELECT id
+    FROM trail
+    WHERE id = '$trailId'
+    ");
 
   $db->query("
-      UPDATE trail
-      SET content = '$content',
-          title = '$title',
-          modified = '$now'
-      WHERE url_slug='$trailId'
-  ");
-  
+    UPDATE trail
+    SET content = '$content',
+    title = '$title',
+    modified = '$now'
+    WHERE url_slug='$trailId'
+    ");
+
   $trailId = "junk";
 }
 else
 {
   // generate a url_slug
   $created = date('Y-m-d H:i:s');
-  
+
   // Generate initial URL slug
   $values = serialize($_POST);
   $length = 4;
   $url_slug = substr(md5($values . time()), 0, $length);
   $exists = $db->value("
+    SELECT id
+    FROM trail
+    WHERE url_slug = '$url_slug'
+    ");
+
+  // Make sure we're not choosing an existing URL slug
+  while ($exists) 
+  {
+    $length++;
+    if ($length == 32) 
+    {
+      // An exact duplicate shift exists
+      echo "{status: 0, message:'Duplicate trail'}";
+      exit;
+    }
+    $url_slug = substr(md5($values . time()), 0, $length);
+    $exists = $db->value("
       SELECT id
       FROM trail
       WHERE url_slug = '$url_slug'
-  ");
-
-  // Make sure we're not choosing an existing URL slug
-  while ($exists) {
-      $length++;
-      if ($length == 32) {
-          // An exact duplicate shift exists
-          echo "{status: 0, message:'Duplicate trail'}";
-          exit;
-      }
-      $url_slug = substr(md5($values . time()), 0, $length);
-      $exists = $db->value("
-          SELECT id
-          FROM trail
-          WHERE url_slug = '$url_slug'
       ");
   }
-  
+
   // insert it
   // Record a general accounting of shift
   $db->query("
-      INSERT INTO trail
-      (user_id, content, title, url_slug, created, modified, status, thumb_status)
-      VALUES ($user->id, '$content', 'title', '$url_slug', '$created', '$modified', '$status', '$thumb_status')
-  ");
-  
+    INSERT INTO trail
+    (user_id, content, title, url_slug, created, modified, status, thumb_status)
+    VALUES ($user->id, '$content', 'title', '$url_slug', '$created', '$modified', '$status', '$thumb_status')
+    ");
+
   $trailId = $url_slug;
 }
 
