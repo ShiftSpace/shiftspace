a77cd8db64b372baee299571214ca220b4a65c4d
david <david>
2008-07-23 20:19:06 +0000

    doc update.

--- client/ShiftSpace.js
+++ client/ShiftSpace.js
@@ -58,7 +58,7 @@ var ShiftSpace = new (function() {
     // Check to see if the server URL is already stored
     // permissions problem here?
     if (typeof server == 'undefined') {
-      var server = getValue('server', 'http://localhost/~josephmoore/');
+      var server = getValue('server', 'http://metatron.shiftspace.org/api/');
     }
     
     //server = "http://localhost/~davidnolen/shiftspace-0.11/";
