32a43b852d7e841f1f6a82bc69458dd2a483c1d7
david <david>
2008-07-29 07:12:22 +0000

    fixed typo in Highlights

--- spaces/Highlights/Highlights.js
+++ spaces/Highlights/Highlights.js
@@ -316,10 +316,6 @@ var HighlightsShift = ShiftSpace.Shift.extend({
   
   setup: function(json) 
   {
-    console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
-    console.log(jsonerm);
-    console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
-
     if(json.ranges)
     {
       //replace __newline__ token with \n
