f0282981789bb2c95ddea97201961efd616633d2
david <david>
2008-07-29 16:56:19 +0000

    Added SSTableViewDatasource class.

---
+++ client/SSTableViewDatasource.js
@@ -0,0 +1,92 @@
+/*
+  Class: SSTableViewDatasource
+    Properties passed to the data providing url should send a json object with the following structure
+    
+    (start code)
+    {
+      ids: '*',
+      users: ['mushon', 'dphiffer'],
+      hrefs: '*',
+      created: '*',
+      summary: '*'
+    }
+    (end)
+*/
+var SSTableViewDatasource = new Class({
+  
+  Implements: Events,
+  
+
+  initialize: function()
+  {
+    // set the default data provider URL
+    this.setDataProviderURL('http://metatron.shiftspace.org/shiftspace.php?method=shift.query');
+  },
+  
+  
+  setData: function(newData)
+  {
+    this.__data__ = newData;
+  },
+  
+  
+  data: function()
+  {
+    return this.__data;
+  },
+  
+  
+  setDataProviderURL: function(url)
+  {
+    this.__url__ = url;
+  },
+  
+  
+  dataProviderURL: function()
+  {
+    return this.__url__;
+  },
+  
+  
+  setProperties: function(props)
+  {
+    this.__properties__ = props;
+  },
+  
+  
+  properties: function()
+  {
+    return this.__properties__;
+  },
+
+
+  rowCount: function()
+  {
+    return this.data().length;
+  },
+  
+
+  itemForRowIndexColumn: function(rowIndex, column)
+  {
+    this.data()[rowIndex][column];
+  },
+  
+
+  sortByColumn: function(column, direction)
+  {
+    this.fetch($merge(
+      this.properties(), 
+      {
+        sortBy:{column:column, direction:ascending}
+      }
+    ));
+  },
+
+  
+  fetch: function(properties)
+  {
+    // make a call
+    this.fireEvent('dataLoaded');
+  }
+
+});
--- client/views/SSTableView/test.js
+++ client/views/SSTableView/test.js
@@ -3,6 +3,7 @@ if(Sandalphon)
   Sandalphon.runTest = function()
   {
     var controller = $$('.SSTableView').retrieve('__ssviewcontroller__');
-    console.log('We got the controller dude');
+    // set the datasource for the tableview
   }
 }
+
