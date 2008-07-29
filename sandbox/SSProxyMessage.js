262fc7f2d2de2ca86e6991afdd517248fc97ea4f
david <david>
2008-07-29 06:19:17 +0000

    oops

--- sandbox/SSProxyMessage.js
+++ sandbox/SSProxyMessage.js
@@ -1,3 +1,68 @@
+var SSProxyMessageTimeout;
+var SSProxyMessageIsVisible = true;
+var SSProxyMessageIsAnimating = false;
+
 window.addEvent('domready', function() {
-  
+  SSProxyMessageInit();
 });
+
+function SSProxyMessageInit()
+{
+  SSProxyMessageTimeout = setTimeout(SSProxyMessageHide, 10000);
+  
+  $('SSProxyMessageTimeout').addEvent('mouseover', function(_evt) {
+    if(!SSProxyMessageIsVisible)
+    {
+      SSProxyMessageShow();
+    }
+    
+    if(SSProxyMessageTimeout)
+    {
+      clearTimeout(SSProxyMessageTimeout);
+    }
+    
+    SSProxyMessageTimeout = setTimeout(SSProxyMessageHide, 10000);
+  });
+}
+
+function SSProxyMessageShow()
+{
+  var showFx = $('SSProxyMessage').effects({
+    duration: 500, 
+    transition: Fx.Transitions.Cubic.easeOut
+    onStart: function()
+    {
+      SSProxyMessageIsAnimating = true;
+    },
+    onComplete: function()
+    {
+      SSProxyMessageIsAnimating = false;
+      SSProxyMessageIsVisible = true;
+    }
+  });
+  
+  showFx.start({
+    right: [0, -503]
+  });
+}
+
+function SSProxyMessageHide()
+{
+  var hideFx = $('SSProxyMessage').effects({
+    duration: 500,
+    transition: Fx.Transitions.Cubic.easeOut,
+    onStart: function()
+    {
+      SSProxyMessageIsAnimating = true;
+    },
+    onComplete: function()
+    {
+      SSProxyMessageIsAnimating = false;
+      SSProxyMessageIsVisible = false;
+    }
+  });
+  
+  hideFx.start({
+    right: [0, -503]
+  });
+}
