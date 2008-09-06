5ec436b8f96c04bd8f4799db2d3c84ee0e4f5b3e
david <david>
2008-09-06 16:46:45 +0000

    Added Comments plugin directory.

---
+++ plugins/Comments/Comments.js
@@ -0,0 +1,31 @@
+var Comments = ShiftSpace.Plugin.extend({
+  pluginType: ShiftSpace.Plugin.types.get('kWindowType'),
+
+  attributes:
+  {
+    name: 'Comments',
+    title: null,
+    icon: null,
+    css: 'Comments.css',
+    version: 0.2
+  },
+  
+  setup: function()
+  {
+    
+  },
+  
+  
+  show: function()
+  {
+    
+  },
+  
+  
+  hide: function()
+  {
+    
+  }
+  
+  
+});
