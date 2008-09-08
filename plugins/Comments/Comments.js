e1fbb94f9c70d192740330bf5005a628d100df47
david <david>
2008-09-08 03:03:46 +0000

    another attempt at overflow check.

--- plugins/Comments/Comments.js
+++ plugins/Comments/Comments.js
@@ -206,14 +206,21 @@ var CommentsPlugin = ShiftSpace.Plugin.extend({
 
     this.minimizer.addEvent('click', this.hideInterface.bind(this));
 
+    var overflow = $(document.body).getStyle('overflow');
+    var windowSize = window.getStyle().size;
+    var windowScrollSize = window.getSize().scroll;
+    var setOverflow = (windowSize.x == windowScrollSize.x && windowSize.y == windowScrollSize.y);
+
     this.element.makeResizable({
       modifiers: {x:'', y:'top'},
       handle: this.comCount,
       onStart: function()
       {
+        if(setOverflow) $(document.body).setStyle('overflow', 'hidden');
       },
       onComplete: function()
       {
+        if(setOverflow) $(document.body).setStyle('overflow', overflow);
       }
     });
     
