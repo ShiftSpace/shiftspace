2b49e4db2d2669b49c43836c0dafedf1a5e42cdf
david <david>
2008-09-22 21:09:19 +0000

    unescape shift content.

--- sandbox/simple_proxy.php
+++ sandbox/simple_proxy.php
@@ -253,7 +253,7 @@ foreach ($shift_ids as $an_id)
     // get the shift out of the database
     $ShiftSpace .= "<script type='text/javascript' charset='utf-8'>
       window.addEvent('domready', function() {
-	var theShift = $shiftContent;
+	var theShift = unescape($shiftContent);
 	
 	if($legacyValue)
 	{
