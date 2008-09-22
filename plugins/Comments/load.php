a0556a9f51f688c2d5195010781ec492996a5698
david <david>
2008-09-22 20:56:26 +0000

    shorten the urls even more.

--- plugins/Comments/load.php
+++ plugins/Comments/load.php
@@ -74,9 +74,9 @@ $ownerGravatar = md5($shift->email);
 $spaceIcon = "http://www.shiftspace.org/dev/images/" . strtolower($shift->space) . "_thumb.png";
 
 $shiftHref = $shift->href;
-if(strlen($shiftHref) > 50)
+if(strlen($shiftHref) > 27)
 {
-  $shiftHref = substr($shiftHref, 0, 47) . '...';
+  $shiftHref = substr($shiftHref, 0, 30) . '...';
 }
 
 $commentsHTML = "
