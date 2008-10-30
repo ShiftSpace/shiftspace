adc43dcc0b2edaad72c9205be4426ef5c235459c
david <david>
2008-10-30 03:34:48 +0000

    Added unit test for SSPackageSorter, turned SSPackageSorter into a Python class.

--- builder/SSPackageSorter.py
+++ builder/SSPackageSorter.py
@@ -1,102 +1,131 @@
-f1 = {'name':'f1'}
-f2 = {'name':'f2'}
-f3 = {'name':'f3'}
-f4 = {'name':'f4'}
-f5 = {'name':'f5'}
-f6 = {'name':'f6'}
-f7 = {'name':'f7'}
-f8 = {'name':'f8'}
-
-f1['deps'] = [f8]
-f2['deps'] = []
-f3['deps'] = []
-f4['deps'] = [f8]
-f5['deps'] = []
-f6['deps'] = [f4]
-f7['deps'] = [f3]
-f8['deps'] = [f2]
-
-pkg = [f1, f2, f3, f4, f5, f6, f7, f8]
 
 # Exceptions ============================== 
 
 class SSPackageSorterError(Exception): pass
-class CircularReferenceError(SSPackageSorterError): pass
-class DelegateDoesNotImplementDependenciesForFile(SSPackageSorterError): pass
+class SSPackageSorterCircularReferenceError(SSPackageSorterError): pass
+class SSInvalidPackageSorterDelegate(SSPackageSorterError): pass
+class SSNoPackageSorterDelegateSet(SSPackageSorterError): pass
 
 # SSPackageSorter =========================
 
 class SSPackageSorter():
-  def __init__(self, packageDelegate):
-    self.__delegate = packageDelegate
+  """
+  Generic class for sorting packages. It takes a packageDelegate
+  which should provide a method called dependenciesFor that returns
+  an array of dependencies for an object.
+  """
+
+  def __init__(self, packageDelegate=None):
+    if packageDelegate != None:
+      self.setDelegate(packageDelegate)
+    self.dependencyStack = []
+
 
   def setDelegate(self, delegate):
+    """
+    Set the package delegate.
+    """
+    try:
+      getattr(delegate, "dependenciesFor")
+    except AttributeError:
+      raise SSInvalidPackageSorterDelegate
+
     self.__delegate = delegate
 
+
   def delegate(self):
+    """
+    Returns the package delegate.
+    """
     return self.__delegate
 
-def depthScore(fileA):
-  # should memo-ize the files
-  val = 0
-  if len(fileA['deps']) == 0:
-    return 0
-  else:
-    for f in fileA['deps']:
-      val = max(val, 1+depthScore(f))
-  return val
+
+  def depthScore(self, fileA):
+    """
+    Returns the depth score for a particular file.
+    """
+
+    if self.delegate() == None:
+      raise SSNoPackageSorterDelegateSet
+
+    # should memo-ize the files
+    deps = self.delegate().dependenciesFor(fileA);
+
+    val = 0
+    if len(deps) == 0:
+      return 0
+    else:
+      for f in deps:
+        val = max(val, 1+self.depthScore(f))
+    return val
+	
+
+  def depthScoresForPackage(self, package):
+    """
+    Returns an array of depth mappings for a package.
+    """
+    return [self.depthScore(f) for f in package]
 
 
-def depthScoresForPackage(package):
-  return [depthScore(f) for f in package]
+  def applyDepthsScoreToPackage(self, package):
+    """
+    Creates a copy with depth metadata attached.
+    """
+    wrappedPackage = [{'object':f} for f in package]
+    depthMap = self.depthScoresForPackage(package)
+ 
+    for i in range(0, len(package)):
+      wrappedPackage[i]['depth'] = depthMap[i]
+
+    return wrappedPackage
 
 
-def applyDepthsScoreToPackage(package):
-  wrappedPackage = [{'object':f} for f in package]
-  depthMap = depthScoresForPackage(package)
-  for i in range(0, len(package)):
-    wrappedPackage[i]['depth'] = depthMap[i]
-  return wrappedPackage
+  def depthSort(self, fileA, fileB):
+    """
+    Used for sorting packages. Return 1, 0, -1 depending of the
+    depedency depth of the two files in question.
+    """
+    return cmp(fileA['depth'], fileB['depth'])
 
 
-def depthSort(fileA, fileB):
-  return cmp(fileA['depth'], fileB['depth'])
+  def isInDependencyTree(self, fileA, fileB):
+    """
+    Checks if the first argument is dependent on the second argument
+    """
 
+    if self.delegate() == None:
+      raise SSNoPackageSorterDelegateSet
 
-def checkForCircularReference(fileA, fileB):
-  return isInDependencyTree(fileA, fileB) and isInDependencyTree(fileB, fileA)
+    # check for circular reference 
+    try:
+      index = self.dependencyStack.index(fileA)
+      raise SSPackageSorterCircularReferenceError
+    except ValueError:
+      self.dependencyStack.append(fileA)
 
-
-# f1, f8 infinite loop
-dependencyStack = []
-def isInDependencyTree(fileA, fileB, orig=None):
-  # add the file to dependency stack
-  global dependencyStack
-
-  try:
-    index = dependencyStack.index(fileA)
-    raise CircularReferenceError
-  except ValueError:
-    dependencyStack.append(fileA)
-
-  if fileA == orig:
-    raise CircularReferenceError
-
-  try:
-    idx = fileA['deps'].index(fileB)
-    return True
-  except ValueError:
-    pass
-
-  for f in fileA['deps']:
-    if isInDependencyTree(f, fileB, orig):
+    # check if the fileA is dependent on fileB directly
+    try:
+      idx = self.delegate().dependenciesFor(fileA).index(fileB)
       return True
+    except ValueError:
+      pass
 
-  return False
+    # check dependecies whether file is in dependency tree
+    for f in self.delegate().dependenciesFor(fileA):
+      if self.isInDependencyTree(f, fileB, orig):
+        return True
+
+    # pop the item off the stack on exit
+    self.dependencyStack.pop()
+
+    return False
 
 
-def sortPackage(package):
-  wrappedPackage = applyDepthsScoreToPackage(package)
-  wrappedPackage.sort(depthSort)
-  return [f['object'] for f in wrappedPackage]
+  def sortPackage(self, package):
+    """
+    Takes a package and returns a sorted copy.
+    """
+    wrappedPackage = self.applyDepthsScoreToPackage(package)
+    wrappedPackage.sort(self.depthSort)
+    return [f['object'] for f in wrappedPackage]
 
