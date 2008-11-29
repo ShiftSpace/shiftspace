6569da3b89beca18f5942ec06ba896dad4ebc477
david <david>
2008-11-29 20:59:52 +0000

    fix for build_test.php that uses command line flags.

--- builder/build_test.php
+++ builder/build_test.php
@@ -5,10 +5,10 @@ $env = $_GET['env'];
 $testsJson = json_decode(file_get_contents('../config/tests.json'), true);
 $fileOrder = $testsJson['dependencies'][$test];
 
-foreach ($fileOrder as $f) {
-  system("python preprocess.py $env shiftspace $f");
+foreach ($fileOrder as $f)
+{
+  system("python preprocess.py -e $env -p shiftspace -i $f");
   echo "\n";
 }
-
 
 ?>
