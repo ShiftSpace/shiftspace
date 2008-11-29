ccbc8ea0c644a9468d539f0ae6a82994d6b3d74b
joe <joe>
2008-11-29 22:29:06 +0000

    moved much of the logic to space. added basic interface code though not in sycn with older mootools

--- spaces/Quote/Quote.js
+++ spaces/Quote/Quote.js
@@ -4,58 +4,196 @@ var QuoteSpace = ShiftSpace.Space.extend({
     icon: 'Quote.png', 
     version: 0.1,
     css: 'spaces/Quote/Quote.css'
-  }
-});
-
-
-var QuoteShift = ShiftSpace.Shift.extend({
-  setup: function(){
-    var self = this;
-    self.addBodyListener();
   },
   
-  addBodyListener: function(){
+  setup: function(){
     var self = this;
-    var bod = document.getElement("body");
-    $(bod).addEvents({  
-      "mouseover": function(e){
-        if(self.isProtectedElement(e.target) == false && !self.quotedElement){
-          self.addOverEffect(e.target);
+    self.buildInterface();
+    
+  },
+  
+  buildInterface: function(){
+    var self = this;
+    //body events
+    self.overElem = function(e){
+      if(self.isProtectedElement(e.target) == false && !self.quotedElement){
+        self.addOverEffect(e.target);
+      }
+    }
+    
+    self.outElem = function(e){
+      if(self.isProtectedElement(e.target) == false && !self.quotedElement){
+        self.removeOverEffect(e.target);
+      }
+    }
+    
+    self.downElem = function(e){
+      //need to grab styles from children elements as well
+      if(self.isProtectedElement(e.target) == false && !self.quotedElement){
+        e.target.setStyle('outline','');
+        self.quotedElement = self.getClone(e.target);
+        
+        // store 
+        console.log(self);
+        self.setValue('SSQ',{'content':'myContent'});
+        
+      }else{
+        
+        self.getValue('SSQ',null,function(SSQ){
+            console.log(SSQ.content);
+        });
+        var container = self.createContainer();
+        container.appendChild(self.quotedElement);
+        container.injectInside(document.body);
+        var handle = container.getElementsByClassName('SSQuoteHandle')[0];
+        container.makeDraggable({'handle':handle});
+        container.setStyles({
+            'top':e.clientY,
+            'left':e.clientX
+        });
+        self.quotedElement = null;
+        self.removeDocumentEvents();
+      }
+    }
+  },
+  
+  addDocumentEvents: function(){
+    var self = this;
+    $(document).addEvents({
+        'mouseover':self.overElem,
+        'mouseout':self.outElem,
+        'mousedown':self.downElem
+    });
+  },
+  
+  removeDocumentEvents: function(){
+    var self = this;
+    $(document).removeEvent('mouseover',self.overElem);
+    $(document).removeEvent('mouseout',self.outElem);
+    $(document).removeEvent('mousedown',self.downElem);
+  },
+  
+  buildWidget: function(){
+    self.SSQuoteWidget = new Element('div',{
+        'id': 'SSQuoteWidget',
+        'styles':{
+          'left':posx,
+          'top':posy
         }
-      },
-      "mouseout":function(e){
-        if(self.isProtectedElement(e.target) == false && !self.quotedElement){
-          self.removeOverEffect(e.target);
+    });
+    
+    self.SSQuoteUI = new Element('div',{
+        'id':'SSQuoteUI'
+    });
+    
+    self.SSQuoteHandle = new Element('div',{
+        'id':'SSQuoteHandle'
+    });
+    
+    self.SSQuoteButton = new Element('a',{
+        'id':'SSQuoteButton'
+    });
+    
+    self.SSQuoteTransparentButton = new Element('a',{
+        'id':'SSQuoteTransparentButton'
+    });
+    
+    self.SSQuoteWhiteButton = new Element('a',{
+        'id':'SSQuoteWhiteButton'
+    });
+    
+    self.SSQuoteBlackButton = new Element('a',{
+        'id':'SSQuoteBlackButton'
+    });
+    
+    self.SSQuoteHandle.injectInside(self.SSQuoteUI);
+    self.SSQuoteButton.injectInside(self.SSQuoteUI);
+    self.SSQuoteTransparentButton.injectInside(self.SSQuoteUI);
+    self.SSQuoteWhiteButton.injectInside(self.SSQuoteUI);
+    self.SSQuoteBlackButton.injectInside(self.SSQuoteUI);
+    self.SSQuoteUI.injectInside(self.SSQuoteWidget)
+    self.SSQuoteWidget.injectInside(document.body);
+    self.attachSSQuoteWidgetEvents();
+    self.attachSSQuoteButtonEvents();      
+  },
+  
+  attachSSQuoteWidgetEvents: function(){
+    var self = this;
+    self.SSQuoteWidget.addEvents({
+        'mouseleave':function(evt){
+          this.setStyle('display','none');
         }
-      },
-      "mousedown":function(e){
-        //need to grab styles from children elements as well
-        if(self.isProtectedElement(e.target) == false && !self.quotedElement){
-          e.target.setStyle('outline','');
-          self.quotedElement = self.getClone(e.target);
-          
-          // store 
-          console.log(self);
-          self.setValue('SSQ',{'content':'myContent'});
-          
-        }else{
-          
-          self.getValue('SSQ',null,function(SSQ){
-              console.log(SSQ.content);
-          });
-          var container = self.createContainer();
-          container.appendChild(self.quotedElement);
-          container.injectInside(document.body);
-          var handle = container.getElementsByClassName('SSQuoteHandle')[0];
-          container.makeDraggable({'handle':handle});
-          container.setStyles({
-              'top':e.clientY,
-              'left':e.clientX
-          });
-          self.quotedElement = null;
+    });
+  },
+  
+  attachSSQuoteButtonEvents: function(){
+    var self = this;
+    
+    var morphObject = new Fx.Morph(self.SSQuoteWidget,{
+        duration:'short'
+    });
+    
+    self.SSQuoteButton.addEvents({
+        'click':function(evt){
+          self.insertQuote(self.SSQuoteWidget);
         }
+    });
+    
+    self.SSQuoteTransparentButton.addEvents({
+        'click':function(evt){
+          self.SSQuoteWidget.setStyle('background-color','transparent');  
+        }
+    });
+    
+    self.SSQuoteWhiteButton.addEvents({
+        'click':function(evt){
+          morphObject.start({'background-color':'#FFFFFF'});
+        }
+    });
+    
+    self.SSQuoteBlackButton.addEvents({
+        'click':function(evt){
+          morphObject.start({'background-color':'#333333'});
+        }
+    });
+  },
+  
+  insertQuote: function(container,quote){
+    var self = this;
+    var quote = new Element('p',{
+        'id':'quote',
+      'html':'lorem ipsum is dummy text dummmy<br />Hello',
+      'styles': {
+        'width':'200px',
+        'height':'150px'
       }
     });
+    
+    container.appendChild(quote);
+    var efx = new Fx.Morph($(container),{
+        duration:'short'
+    });
+    
+    efx.start({
+        'width':quote.getComputedStyle('width') ,
+        'height':quote.getComputedStyle('height')
+    });
+  
+    container.removeEvents('mouseleave');
+    self.SSQuoteButton.removeEvents('click');
+    var myDrag = new Drag($(container),{'handle':$('SSQuoteHandle')});
+    myDrag.attach();
+  },
+  
+  showInterface: function(){
+    var self = this;
+    self.addDocumentEvents();
+    self.buildWidget();
+  },
+  
+  hideInterface: function(){
+    var self = this;
+    self.removeDocumentEvents();
   },
   
   isProtectedElement: function(elem){
@@ -84,6 +222,7 @@ var QuoteShift = ShiftSpace.Shift.extend({
     }));
     return container;
   },
+  
   isContained: function(elem){
     var parent = $(elem.getParent());
     while(parent != null)
@@ -93,12 +232,15 @@ var QuoteShift = ShiftSpace.Shift.extend({
     }
     return false;
   },
+  
   addOverEffect: function(elem){
     elem.setStyle('outline','1px solid red');
   },
+  
   removeOverEffect: function(elem){
     elem.setStyle('outline','');
   },
+  
   getClone: function(elem){
     /*does this work on Webkit?*/
     var styles = document.defaultView.getComputedStyle(elem,null);
@@ -115,7 +257,12 @@ var QuoteShift = ShiftSpace.Shift.extend({
     }
     clone.setStyle('outline','');
     return clone;
-  }
+  }   
+});
+
+
+var QuoteShift = ShiftSpace.Shift.extend({
+    
 });
 
 var Quote = new QuoteSpace(QuoteShift);
