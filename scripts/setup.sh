fd0584f6304b323dbb0a4811f3a66e9fa9685471
avital <avital@thewe.net>
2008-12-01 17:27:46 -0500

    Added some important text message

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,21 @@
 # make the build, and compiledViews diretories and set their permissions
 python ../builder/corebuilder.py
 python ../sandalphon/sandalphon.py ../client/views/SSConsole/SSConsole.html
+
+echo
+echo
+echo
+echo
+echo
+echo =============================================================================
+echo A rabbi, a priest and a javascript programmer are sitting in a coffee shop...
+echo =============================================================================
+echo
+echo
+echo
+echo
+echo
+
 ./build_ss.sh
 ./build_sb.sh
 ./build_sd.sh
