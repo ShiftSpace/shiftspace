ecf78abf704db1a767827083b70710c8f0659a1e
avital <avital@thewe.net>
2008-12-01 17:20:54 -0500

    changed executable permission and fixed type on build_sd.sh

--- scripts/build_sd.sh
+++ scripts/build_sd.sh
@@ -1,1 +1,1 @@
-python ../builder/preprocess.py -e sandalphon -i ../client/BootStrap.js -p sandalphon -o ../builds/shiftspace.sandalphon.js -x
+python ../builder/preprocess.py -e sandalphon -i ../client/Bootstrap.js -p sandalphon -o ../builds/shiftspace.sandalphon.js -x
