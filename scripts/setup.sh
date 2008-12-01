00faa58ac8e9131688e7c1eeb1afd9f0a35f3330
David Nolen <davidnolen@david-nolens-macbook-pro.local>
2008-12-01 17:23:44 -0500

    get setup.sh script working.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,6 @@
 # make the build, and compiledViews diretories and set their permissions
+python ../builder/corebuilder.py
+python ../sandalphon/sandalphon.py ../client/views/SSConsole/SSConsole.html
+./build_ss.sh
+./build_sb.sh
+./build_sd.sh
