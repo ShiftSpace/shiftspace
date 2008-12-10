97afb5326642f1389ca0c2fa9fbe6f806e5527f8
David Nolen <david.nolen@gmail.com>
2008-12-10 17:27:11 -0500

    basic outline for class.

---
+++ client/views/SSForm/SSForm.js
@@ -0,0 +1,44 @@
+// ==Builder==
+// @uiclass
+// @required
+// @package           ShiftSpaceCoreUI
+// @dependencies      SSView
+// ==/Builder==
+
+var SSForm = new Class({
+
+  Extends: SSView,
+
+  name: "SSForm",
+
+  initialize: function(el, options)
+  {
+    this.parent(el, options);
+
+    
+  },
+  
+  initSubForm: function()
+  {
+    this.element.getElements('.SSFormStep').each(function(subForm) {
+      // prevent default submit behavior
+      subForm.addEvent('submit', function(_evt) {
+        var evt = new Event(_evt);
+        evt.preventDefault();
+      }.bind(this));
+      
+      this.initSubmitButtonForSubForm(aStep, aStep.getElement('input[submit]'));
+    });
+  },
+  
+  initSubmitButtonForSubForm: function(step, button)
+  {
+    
+  },
+  
+  validateSubForm: function(subForm)
+  {
+    
+  }
+
+});
