0cc688e8047032596aef3d63df004406fb9c9376
David Nolen <david.nolen@gmail.com>
2008-12-30 14:31:12 -0500

    made build_moma_sandbox.sh create the markup and css files as well.

--- scripts/build_moma_sandbox.sh
+++ scripts/build_moma_sandbox.sh
@@ -1,1 +1,2 @@
+python ../sandalphon/sandalphon.py ../momasocialbar/views/MoMAConsole/MoMAConsole.html;
 python ../builder/preprocess.py -e dev -i ../momasocialbar/BootstrapMoMA.js -p MoMA -o ../builds/shiftspace.sandbox.js;
