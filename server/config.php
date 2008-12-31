d972cabbb6ef46815e6c656c786246b9c7e0cca9
David Nolen <david.nolen@gmail.com>
2008-12-31 15:58:38 -0500

    Removing old server directory.

--- server/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$db_path = "sqlite://$dir/shiftspace.sqlite3";
-$purify_content = false;
-
-?>
--- server/methods/user.logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$_SESSION['user'] = null;
-$expires = time() - 60 * 60 * 24 * 365;
-setcookie('auth', "", $expires, '/');
-
-?>
