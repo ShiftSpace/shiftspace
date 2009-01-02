999e1ba3fb29f2934a71926b391b2c4eb7c10ec9
David Nolen <david.nolen@gmail.com>
2009-01-02 16:13:54 -0500

    fleshing out the SSMultiViewTest.

--- tests/SSMultiViewTest/SSMultiViewTest.js
+++ tests/SSMultiViewTest/SSMultiViewTest.js
@@ -21,17 +21,50 @@ var SSMultiViewTest = new Class({
   },
   
   
-  testShowSubView: function()
+  testShowView: function()
   {
-    this.doc("Test showing a subview wether controller backed or not");
+    this.doc("Test showing a subview by index");
     
     var hook = this.startAsync();
+
     Sandalphon.compileAndLoad('tests/SSMultiViewTest/SSMultiViewTest1', function(ui) {
+      
       Sandalphon.addStyle(ui.styles);
       $('SSTestRunnerStage').set('html', ui.interface);
       Sandalphon.activate($('SSTestRunnerStage'));
+      
       var multiview = SSControllerForNode($('SSMultiViewTest'));
+      multiview.showView(1);
+      var allSubViews = multiview.element.getElements('.SSSubView');
+
+      this.assertEqual(1, allSubViews.indexOf(multiview.element.getElement('.SSActive')), hook);
+      
       this.endAsync(hook);
+      
+    }.bind(this));
+  },
+  
+  
+  testShowViewByName: function()
+  {
+    this.doc("Test showing a subview by name");
+    
+    var hook = this.startAsync();
+
+    Sandalphon.compileAndLoad('tests/SSMultiViewTest/SSMultiViewTest1', function(ui) {
+      
+      Sandalphon.addStyle(ui.styles);
+      $('SSTestRunnerStage').set('html', ui.interface);
+      Sandalphon.activate($('SSTestRunnerStage'));
+      
+      var multiview = SSControllerForNode($('SSMultiViewTest'));
+      multiview.showViewByName('SSMultiViewSubView2');
+      var allSubViews = multiview.element.getElements('.SSSubView');
+
+      this.assertEqual(1, allSubViews.indexOf(multiview.element.getElement('.SSActive')), hook);
+      
+      this.endAsync(hook);
+      
     }.bind(this));
   },
   
@@ -39,6 +72,8 @@ var SSMultiViewTest = new Class({
   testSubViewSelector: function()
   {
     this.doc("Test using a different subview CSS selector than .SSSubView");
+    
+    
   }
   
 });
