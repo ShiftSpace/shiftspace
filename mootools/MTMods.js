8946a8892ff9d547455066a6fb7ab5aed7f45439
David Nolen <david.nolen@gmail.com>
2009-01-21 14:50:24 -0500

    added Array copy over from our copy of mt-core.

--- mootools/MTMods.js
+++ mootools/MTMods.js
@@ -8,6 +8,14 @@ String.implement({
   }
 });
 
+Array.implement({
+  copy: function(){
+    var results = [];
+    for(var i = 0, l = this.length; i < l; i++) results[i] = this[i];
+    return results;
+  },
+});
+
 var IFrame = new Native({
 
   name: 'IFrame',
