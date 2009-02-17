4dfc937b1010226d295f53f027b15e4ed8e33256
David Nolen <david.nolen@gmail.com>
2009-02-16 19:48:26 -0500

    loading IE7.js now.

--- sandbox/index.php
+++ sandbox/index.php
@@ -39,6 +39,7 @@ $server = "http://{$_SERVER['HTTP_HOST']}$path/";
   	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.5.3/jquery-ui.js" type="text/javascript" charset="utf-8"></script>
     -->
     <link rel="stylesheet" href="style.css" type="text/css"></link>
+    <script src="http://ie7-js.googlecode.com/svn/version/2.0(beta3)/IE7.js" type="text/javascript" charset="utf-8"></script>
     <script src="../mootools/mootools-1.2-core.js" type="text/javascript"></script>
     <script src="../mootools/mootools-1.2-more.js" type="text/javascript"></script>
     <script src="GreaseMonkeyApi.js" type="text/javascript"></script>
