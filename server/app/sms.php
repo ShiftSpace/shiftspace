73a806c9286890385b9745a4b175da8f6c00b157
avital <avital@thewe.net>
2009-02-18 18:08:41 -0500

    Another SMS Test

--- server/app/sms.php
+++ server/app/sms.php
@@ -1,5 +1,8 @@
 <?php
 
+class Artwork_Object extends Base_Object {
+}
+
 class Sms {
   public function __construct($server) {
     $this->server = $server;
@@ -13,7 +16,7 @@ class Sms {
   
   public function receive() {
     extract($_POST);
-
+        
     $f = fopen(dirname(__FILE__)."/sms.log", "a");
     fwrite($f, "Phone: $phone\n");
     fwrite($f, "Message: $msg\n");
@@ -21,6 +24,11 @@ class Sms {
     fwrite($f, "=====\n\n");
     fflush($f);
     fclose($f);
+
+    $artworkid = trim($msg);
+    $artwork = $this->server->moma->load("artwork($artworkid)")->get();
+    $title = $artwork['title'];
+    return "Hey there. $title was just saved for you. Go to txt.moma.org to retrieve it and any other works you collect. See you there!";
   }
 }
 
