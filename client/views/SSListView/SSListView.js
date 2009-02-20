483550334f53ac6e151ae62dbb6def1aaeca14c1
David Nolen <david.nolen@gmail.com>
2009-02-19 20:13:55 -0500

    erg

--- client/views/SSListView/SSListView.js
+++ client/views/SSListView/SSListView.js
@@ -549,7 +549,9 @@ var SSListView = new Class({
     var delegate = this.delegate();
     
     var canRemove = (delegate && delegate.canRemove && delegate.canRemove({listView:this, index:index})) || null;
+    console.log('canRemove ' + canRemove);
     if(canRemove == null) canRemove = true;
+    console.log('canRemove ' + canRemove);
 
     if(canRemove)
     {
