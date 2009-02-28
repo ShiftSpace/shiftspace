3c76cca2b9789f5c62cdfb75b9403742321bf028
David Nolen <david.nolen@gmail.com>
2009-02-28 01:49:34 -0500

    exit if explorer version < 7.

--- config/main/MoMAMain.js
+++ config/main/MoMAMain.js
@@ -3,5 +3,6 @@ window.addEvent('domready', InitMoMASocialBar);
 var Console;
 function InitMoMASocialBar()
 {
+  if(Browser.Engine.trident && (Browser.Engine.version < 7)) return;
   Console = new MoMAConsole();
 }
