60fb189f43a9152d7ec625373bc3383087e0fb77
avital <avital@thewe.net>
2009-02-28 02:12:05 -0500

    I think this fixes the short timeout bug

--- server/index.php
+++ server/index.php
@@ -2,6 +2,9 @@
 
 $dir = dirname(__FILE__);
 
+// 1 month
+ini_set('session.gc_maxlifetime', 2952000);
+
 require_once "$dir/library/base.php";
 try {
   $server = Base_Server::singleton('server.ini', 'working/server.ini');
