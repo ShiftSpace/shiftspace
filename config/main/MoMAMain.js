f85eeee9e3fd5494168b39b517045578fbe3e796
David Nolen <david.nolen@gmail.com>
2009-03-01 20:11:13 -0500

    load in IE7

--- config/main/MoMAMain.js
+++ config/main/MoMAMain.js
@@ -3,6 +3,9 @@ window.addEvent('domready', InitMoMASocialBar);
 var Console;
 function InitMoMASocialBar()
 {
-  if(Browser.Engine.trident && (Browser.Engine.version < 7)) return;
+  if(Browser.Engine.trident && (Browser.Engine.version < 5))
+  {
+    return;
+  }
   Console = new MoMAConsole();
 }
