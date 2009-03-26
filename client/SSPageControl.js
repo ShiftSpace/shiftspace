74407566a20db73d22e61bc5911ab13fb5985f9e
David Nolen <david.nolen@gmail.com>
2009-03-26 17:01:59 -0400

    fleshing out page control logic.

--- client/SSPageControl.js
+++ client/SSPageControl.js
@@ -11,7 +11,8 @@ var SSPageControl = new Class({
   
 
   defaults: {
-    listView: null
+    listView: null,
+    perpage: 25
   }, 
 
 
@@ -25,6 +26,9 @@ var SSPageControl = new Class({
     if(options.listView)
     {
       this.setListView(options.listView);
+      
+      // set a filter
+      this.listView().setFilter(this.filterItem.bind(this));
 
       if(options.listView.dataIsReady())
       {
@@ -32,6 +36,49 @@ var SSPageControl = new Class({
       }
       this.listView().addEvent('onReloadData', this.initalizeInterface.bind(this));
     }
+  },
+  
+  
+  filterItem: function(x, index)
+  {
+    return (index >= this.lowerBound()) || (index <= this.upperBound());
+  },
+  
+  
+  setCurrentPage: function(page)
+  {
+    this.__currentPage = page;
+    this.listView().refresh(true);
+  },
+  
+  
+  currentPage: function()
+  {
+    return this.__currentPage;
+  },
+  
+  
+  setPerPage: function(perpage)
+  {
+    this.__perpage = perpage;
+  },
+  
+  
+  perPage: function()
+  {
+    return this.__perpage;
+  },
+  
+  
+  lowerBound: function()
+  {
+    return this.currentPage() * this.perPage();
+  },
+  
+  
+  upperBound: function()
+  {
+    return this.lowerBound() + this.perPage();
   },
   
   
@@ -62,6 +109,30 @@ var SSPageControl = new Class({
   initalizeInterface: function()
   {
     // initialize the page control
+    var count = this.listView().count();
+    var numPages = count % this.perPage();
+    
+    this.element.getElements('.page').each(function(x) {
+      var idx = this.element.getElements('.page').indexOf(x);
+      
+      // hide page that beyond the numbe available
+      if(idx > numPages)
+      {
+        x.addClass('SSDisplayNone');
+        if(x.nextSibling()) x.nextSibling().addClass('SSDisplayNone');
+      } 
+      else
+      {
+        // set up the click event
+        x.removeEvents('click');
+        x.addEvent('click', function(_evt) {
+          var evt = new Event(_evt);
+          x.element.getElements('.page').removeClass('SSActive');
+          x.addClass('SSActive');
+          this.setCurrentPage(idx);
+        }.bind(this));
+      }
+    });
   },
   
   
