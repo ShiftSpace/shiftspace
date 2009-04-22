4e07c2d120580b976fc87d275578a715fa40ffc5
bitri <novatheory@novatheory-2.local>
2009-04-22 19:27:57 -0400

    SSTabViewTest in progress

--- tests/SSTabViewTest/SSTabViewTest.js
+++ tests/SSTabViewTest/SSTabViewTest.js
@@ -18,6 +18,9 @@ var SSTabViewTest = new Class({
     console.log('running teardown!');
   },
   
+    /*
+   Tests: indexOfTabByName, indexOfTabByName
+   */
   testIndexOfTabByName: function(){
     
      this.doc("get index of a tab by id name");
@@ -47,7 +50,10 @@ var SSTabViewTest = new Class({
      }.bind(this));
     
   },
-  
+   
+   /*
+   Tests: selectedTab
+   */
   testSelectTab: function(){
 
       this.doc("select tab by id name");
@@ -80,6 +86,9 @@ var SSTabViewTest = new Class({
 
    },
    
+   /*
+   Tests: tabButtonForName, tabButtonForIndex
+   */
    testTabButtonDOM: function(){
 
        this.doc("get DOM node of tab by name/index");
@@ -105,13 +114,43 @@ var SSTabViewTest = new Class({
          this.assertNotEqual(selectedTabIndex1, undefined, hook);
          var selectedTabIndex2 = tabView.tabButtonForIndex(1);
          this.assertNotEqual(selectedTabIndex2, undefined, hook);
-         var selectedTabIndex2 = tabView.tabButtonForIndex(2);
-         this.assertNotEqual(selectedTabIndex2, undefined, hook);
+         var selectedTabIndex3 = tabView.tabButtonForIndex(2);
+         this.assertNotEqual(selectedTabIndex3, undefined, hook);
 
          this.endAsync(hook);
 
        }.bind(this));
 
-    },
+    }, 
+     /*
+     Tests: tabButtonForName, tabButtonForIndex
+     */
+     testContentViewIndex: function(){
+
+         this.doc("get index of contentView by name");
+ 
+         var hook = this.startAsync();
+
+         Sandalphon.compileAndLoad('tests/SSTabViewTest/SSTabViewTest1', function(ui) {
+
+           Sandalphon.addStyle(ui.styles);
+           $('SSTestRunnerStage').set('html', ui.interface);
+           Sandalphon.activate($('SSTestRunnerStage'));
+
+           var tabView = SSControllerForNode($('SSTabViewTest'));
+
+           var selectedTab1 = tabView.indexOfContentView("SSTabPane1");
+           this.assertNotEqual(selectedTabIndex1, 0, hook);
+           var selectedTab2 = tabView.indexOfContentView("SSTabPane2");
+           this.assertNotEqual(selectedTabIndex2, 1, hook);
+           var selectedTab3 = tabView.indexOfContentView("SSTabPane3");
+           this.assertNotEqual(selectedTabIndex3, 2, hook);
+           
+           this.endAsync(hook);
+
+         }.bind(this));
+
+      }
+
   
 });
