68f6bbaa35366b7b1b8bf5ca0cbcb794145ce3b0
bitri <novatheory@novatheory-2.local>
2009-05-13 18:39:22 -0400

    HelloWorldalphon space.

---
+++ spaces/HelloWorldalphon/HelloWorldalphon.js
@@ -0,0 +1,46 @@
+var HelloWorldalphon = new Class({
+  
+    Extends: ShiftSpace.Space,
+    
+    attributes: {
+        name: 'HelloWorldalphon',
+        icon: 'HelloWorld.png',
+        css:  'HelloWorldalphon.css'
+    }
+});
+
+var HelloWorldalphonShift = new Class({
+  
+    Extends: ShiftSpace.Shift,
+  
+    setup: function(json) {
+        this.build(json);
+        this.save();
+        this.manageElement(this.element);
+        Sandalphon.reset();
+    },
+    
+    build: function(json) {
+      Sandalphon.compileAndLoad('spaces/HelloWorldalphon/HelloWorldalphon', function(ui) {
+        Sandalphon.addStyle(ui.styles);
+      });
+    },
+    
+    changeMessage: function() {
+        var msg = prompt("Please enter a new message:", this.messageValue);
+        this.messageValue = msg;
+        this.element.setHTML(msg);
+        this.save();
+    },
+    
+    encode: function() {
+        var pos = this.element.getPosition();
+        return {
+            summary: this.messageValue,
+            message: this.messageValue,
+            position: pos
+        };
+    }
+});
+
+var HelloWorldalphon = new HelloWorldalphon(HelloWorldalphonShift);
