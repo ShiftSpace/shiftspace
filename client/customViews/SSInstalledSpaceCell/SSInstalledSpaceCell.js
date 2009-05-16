7d57ac84e81396faa0e57676931a99576d8c1582
David Nolen <david.nolen@gmail.com>
2009-05-16 15:47:06 -0400

    working in installed space cell.

---
+++ client/customViews/SSInstalledSpaceCell/SSInstalledSpaceCell.js
@@ -0,0 +1,34 @@
+// ==Builder==
+// @uiclass
+// @optional
+// @package           ShiftSpaceUI
+// @dependencies      SSCell
+// ==/Builder==
+
+var SSInstalledSpaceCell = new Class({
+
+  Extends: SSCell,
+
+  name: "SSInstalledSpaceCell",
+  
+  awake: function(context)
+  {
+    this.mapOutletsToThis();
+  },
+  
+  setImage: function(image)
+  {
+    
+  },
+  
+  setName: function(name)
+  {
+    
+  },
+
+
+  setAutolaunch: function(name)
+  {
+    
+  }
+});
