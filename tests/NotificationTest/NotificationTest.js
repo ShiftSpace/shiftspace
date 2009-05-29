739fc31e2d7dd92ef8343437ad1277d08d06c85a
David Nolen <david.nolen@gmail.com>
2009-05-29 14:39:03 -0400

    Added notification unit test.

---
+++ tests/NotificationTest/NotificationTest.js
@@ -0,0 +1,36 @@
+// ==Builder==
+// @test
+// @suite             Core
+// ==/Builder==
+
+var NotificationTest = new Class({
+  
+  name: 'NotificationTest',
+
+  Extends: SSUnitTest.TestCase,
+
+  
+  testAddObserver: function()
+  {
+    
+  },
+  
+  
+  testRemoveObserver: function()
+  {
+    
+  },
+  
+  
+  testFlushObserverQueue: function()
+  {
+    
+  },
+  
+  
+  testPostNotification: function()
+  {
+    
+  }
+
+});
