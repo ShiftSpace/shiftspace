32973b061b6053a113958a41144a8fcc02590034
Clint Newsom <clintnewsom@clint-newsoms-macbook-pro-3.local>
2009-06-02 20:48:31 -0400

    output buffering

--- server/ss-call.php
+++ server/ss-call.php
@@ -3,17 +3,21 @@
 $dir = dirname(__FILE__);
 require_once "$dir/library/base.php";
 
+
 function sscall($args) {
   $_GET = $args;
   $_REQUEST = $args;
   $_POST = $args;
-  
-  try {
-    $server = Base_Server::singleton('server.ini', 'working/server.ini');
-    $server->main();
-  } catch (Exception $e) {
-    echo 'Error: ' . $e->getMessage();
-  }
+
+  ob_start();
+
+  $server = Base_Server::singleton('server.ini', 'working/server.ini');
+  $server->main();
+  $result = ob_get_contents();
+
+  ob_end_clean();
+
+  return $result;
 }  
 
 ?>
