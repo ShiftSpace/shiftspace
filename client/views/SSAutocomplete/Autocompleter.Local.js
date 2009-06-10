077fa38a62265a52f9c0522c05dc558b7fe2055f
Avital Oliver <avital@thewe.net>
2009-06-10 23:17:59 +0000

    autocomplete

---
+++ client/views/SSAutocomplete/Autocompleter.Local.js
@@ -0,0 +1,37 @@
+// ==Builder==
+// @uiclass
+// @package           Autocomplete
+// @dependencies      Autocompleter
+// ==/Builder==
+
+/**
+ * Autocompleter.Local
+ *
+ * http://digitarald.de/project/autocompleter/
+ *
+ * @version		1.1.2
+ *
+ * @license		MIT-style license
+ * @author		Harald Kirschner <mail [at] digitarald.de>
+ * @copyright	Author
+ */
+
+Autocompleter.Local = new Class({
+
+	Extends: Autocompleter,
+
+	options: {
+		minLength: 0,
+		delay: 200
+	},
+
+	initialize: function(element, tokens, options) {
+		this.parent(element, options);
+		this.tokens = tokens;
+	},
+
+	query: function() {
+		this.update(this.filter());
+	}
+
+});
---
+++ client/views/SSAutocomplete/SSAutocomplete.js
@@ -0,0 +1,32 @@
+// ==Builder==
+// @uiclass
+// @package           Autocomplete
+// @dependencies      SSView, Autocompleter
+// ==/Builder==
+
+var SSAutocomplete = new Class({
+  name: 'SSAutocomplete',
+  Extends: SSView,
+
+  initialize: function(el, options)
+  {
+    this.parent(el, options);
+    new Autocompleter.Local(el, ['Avital', 'David', 'Joe', 'Dan', 'Mushon', 'Florica', 'Justin', 'Clint', 'Alex'], {
+      'minLength': 1, // We wait for at least one character
+      'overflow': true // Overflow for more entries
+    });
+              
+    alert(el)
+  },
+
+  setValue: function(value)
+  {
+    if(this.element) this.element.setProperty('value', value);
+  },
+
+  value: function()
+  {
+    if(this.element) return this.element.getProperty('value');
+    return null;
+  }
+});
