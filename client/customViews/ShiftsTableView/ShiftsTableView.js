c4e357b977de506389d88dc96d2f4a7b91870dce
David Nolen <david.nolen@gmail.com>
2009-06-12 15:41:57 -0400

    adding controller for ShiftsTableView

---
+++ client/customViews/ShiftsTableView/ShiftsTableView.js
@@ -0,0 +1,19 @@
+// ==Builder==
+// @uiclass
+// @customView
+// @optional
+// @package           ShiftSpaceUI
+// @dependencies      SSTableView
+// ==/Builder==
+
+var ShiftsTableView = new Class({
+
+  Extends: SSView,
+  name: "ShiftsTableView",
+
+  initialize: function(el, options)
+  {
+    this.parent(el, options);
+  }
+
+});
