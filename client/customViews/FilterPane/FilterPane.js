520c470f449cb69d2f6a6ceac5ef3834248a0625
David Nolen <david.nolen@gmail.com>
2009-06-12 18:39:45 -0400

    added filter pane, styles to support filter pane and publish pane in the console.

---
+++ client/customViews/FilterPane/FilterPane.js
@@ -0,0 +1,19 @@
+// ==Builder==
+// @uiclass
+// @customView
+// @optional
+// @package           ShiftSpaceUI
+// @dependencies      SSView
+// ==/Builder==
+
+var FilterPane = new Class({
+
+  Extends: SSView,
+  name: "FilterPane",
+
+  initialize: function(el, options)
+  {
+    this.parent(el, options);
+  }
+
+});
