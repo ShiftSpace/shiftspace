4319042b958672175a69aa758a5e0203cc2573a8
David Nolen <david.nolen@gmail.com>
2009-06-20 16:32:15 -0400

    fixes for dev userscript build. rewrite @require.

--- scripts/build_dev.sh
+++ scripts/build_dev.sh
@@ -1,1 +1,2 @@
-python ../builder/preprocess.py -e devtest -i ../client/ShiftSpace.js -o ../builds/shiftspace.dev.user.js;
+python ../sandalphon/sandalphon.py -i ../client/views/SSConsole/SSConsole.html -o ../client/compiledViews/
+python ../builder/preprocess.py -e devtest -i ../client/ShiftSpace.js -p shiftspace -o ../builds/shiftspace.dev.user.js;
