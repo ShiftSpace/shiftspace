180a0530ec57fd5859c51c5dd8eb5e2dd46d73a2
David Nolen <david.nolen@gmail.com>
2009-06-25 17:10:18 -0400

    functions to correspond with the backend.

--- client/core/Stream.js
+++ client/core/Stream.js
@@ -2,12 +2,57 @@
 // @required
 // ==/Builder==
 
+function SSGetAllStreams()
+{
+  
+}
+
+function SSGetStream(streamId)
+{
+  
+}
+
 function SSCreateStream(callback)
 {
   
 }
 
+function SSSetStreamPermission(streamId, userId, level)
+{
+  
+}
+
+function SSFindStreams(ref)
+{
+  
+}
+
 function SSSubscriptionsForLoggedInUser()
+{
+  
+}
+
+function SSSubscribeLoggedInUser(streamId)
+{
+  
+}
+
+function SSUnsubscribeLoggedInUser(streamId)
+{
+  
+}
+
+function SSPostEventToStream(streamId, displayString, ref, hasReadStatus)
+{
+  
+}
+
+function SSMarkEventReadForLoggedInUser(eventId)
+{
+  
+}
+
+function SSMarkEventUneadForLoggedInUser(eventId)
 {
   
 }
