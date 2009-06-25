331c3c8221838e82ec6ed0b59ba5fd9dcaf58ad3
David Nolen <david.nolen@gmail.com>
2009-06-25 17:38:12 -0400

    all stream calls.

--- client/core/Stream.js
+++ client/core/Stream.js
@@ -4,16 +4,17 @@
 
 function SSGetAllFeeds(callback)
 {
+  SSStreamCall('event.feed', callback);
 }
 
 function SSGetFeed(streamId, callback)
 {
-  
+  SSStreamCall('event.onefeed', {stream_id:streamId}, callback);
 }
 
 function SSCreateStream(callback)
 {
-  SSStreamCall('event.createstream', callback);
+  SSStreamCall('event.createstream', null, callback);
 }
 
 function SSSetStreamPermission(streamId, userId, level, callback)
@@ -34,32 +35,45 @@ function SSFindStreams(ref, callback)
 
 function SSSubscriptionsForLoggedInUser(callback)
 {
-  
+  SSStreamCall('event.subscriptions', null, callback);
 }
 
 function SSSubscribeLoggedInUser(streamId, callback)
 {
-  
+  SSStreamCall('event.subscribe', {
+    stream_id: streamId
+  }, callback);
 }
 
 function SSUnsubscribeLoggedInUser(streamId, callback)
 {
-  
+  SSStreamCall('event.unsubscribe', {
+    stream_id: streamId
+  }, callback);
 }
 
 function SSPostEventToStream(streamId, displayString, ref, hasReadStatus, callback)
 {
-  
+  SSStreamCall('event.post', {
+    stream_id: streamId,
+    display_string: displayString,
+    object_ref: ref,
+    has_read_status: hasReadStatus
+  }, callback);
 }
 
 function SSMarkEventReadForLoggedInUser(eventId, callback)
 {
-  
+  SSStreamCall('event.markread', {
+    event_id: eventId
+  }, callback);
 }
 
 function SSMarkEventUneadForLoggedInUser(eventId, callback)
 {
-  
+  SSStreamCall('event.markunread', {
+    event_id: eventId
+  }, callback);
 }
 
 function SSStreamCall(method, params, callback)
