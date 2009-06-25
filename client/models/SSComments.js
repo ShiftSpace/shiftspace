50d8f188028ad77c97dd1a56548d96aa22de3095
David Nolen <david.nolen@gmail.com>
2009-06-25 17:57:22 -0400

    added SSComments.js

---
+++ client/models/SSComments.js
@@ -0,0 +1,32 @@
+// ==Builder==
+// @optional
+// @package           ShiftSpaceCore
+// @dependencies      SSAbstractStream
+// ==/Builder==
+
+var SSCommentsClass = new Class({
+
+  Extends: SSAbstractStream,
+  name: "SSComments",
+
+  initialize: function()
+  {
+    
+  },
+  
+  commentsForObject: function(ref)
+  {
+    
+  },
+  
+  addCommentToObject: function(ref)
+  {
+    
+  },
+  
+  removeCommentFromObject: function(ref)
+  {
+    
+  }
+
+});
