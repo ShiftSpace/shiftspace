605b11bb83eb29be6773a8fd73f4ffd1e5645197
David Nolen <david.nolen@gmail.com>
2009-07-22 19:07:39 -0400

    rounding SSCollection calls in SSAbstractStream.

--- client/models/SSAbstractStream.js
+++ client/models/SSAbstractStream.js
@@ -107,35 +107,60 @@ var SSAbstractStream = new Class({
   },
   
   
-  postEvent: function(options)
+  postEvent: function(_options)
   {
-    SSPostEventToStream(this.id(),
-                        options.displayString,
-                        options.userId,
-                        options.userName,
-                        options.objectRef,
-                        options.hasReadStatus,
-                        options.url,
-                        this.onPostEvent.bind(this));
+    if(this.coll())
+    {
+      var options = {
+        display_string: options.displayString,
+        user_id: options.userId,
+        user_name: options.userName,
+        object_ref: options.objectRef,
+        has_read_status: options.hasReadStatus,
+        url: url
+      };
+      
+      this.coll().create(options, this.onPostEvent.bind(this));
+    }
   },
   
   
-  onPostEvent: function(json)
+  onPostEvent: function(evt)
   {
-    
+    SSLog('onPostEvent', SSLogForce);
+    SSLog(evt, SSLogForce);
   },
   
   
-  deleteEvent: function(options)
+  updateEvent: function(id, data)
   {
-    SSDeleteEvent(options.id,
-                  this.onDeleteEvent.bind(this))
+    if(this.coll())
+    {
+      this.coll().udpateById(id, data, this.onUpdateEvent.bind(this));
+    }
+  },
+  
+
+  onUpdateEvent: function(evt)
+  {
+    SSLog('onUpdateEvent', SSLogForce);
+    SSLog(evt, SSLogForce);
   },
   
   
-  onDeleteEvent: function(json)
+  deleteEvent: function(id)
   {
-    
+    if(this.coll())
+    {
+      this.coll().deleteById(id, this.onDeleteEvent.bind(this));
+    }
+  },
+  
+  
+  onDeleteEvent: function(evt)
+  {
+    SSLog('onDeleteEvent', SSLogForce);
+    SSLog(evt, SSLogForce);
   },
   
   
