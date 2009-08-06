f33cf666607bbeb91b87458566fb4154cc0653e0
David Nolen <david.nolen@gmail.com>
2009-08-06 02:58:51 -0400

    deleted server.

--- server/setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import ConfigParser, os
-
-def listdirfull(dir):
-  return [dir + '/' + filename for filename in os.listdir(dir)]
-
-config = ConfigParser.RawConfigParser()
-config.read(listdirfull('config'))
-config.write(open('server.ini', 'w'))
--- server/ss-call-sample.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dir = dirname(__FILE__);
-require_once "$dir/ss-call.php";
-
-sscall(Array('method' => 'version'))
-
-?>
