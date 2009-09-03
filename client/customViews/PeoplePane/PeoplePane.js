10e0dace139e20719dd7fd6b47bd1fa9f6dff0e5
David Nolen <david.nolen@gmail.com>
2009-09-02 20:56:27 -0400

    added PeoplePane, css tweaks.

---
+++ client/customViews/PeoplePane/PeoplePane.js
@@ -0,0 +1,24 @@
+// ==Builder==
+// @uiclass
+// @customView
+// @optional
+// @package           ShiftSpaceUI
+// @dependencies      SSView
+// ==/Builder==
+
+var PeoplePane = new Class({
+
+  Extends: SSView,
+  name: "PeoplePane",
+
+  initialize: function(el, options)
+  {
+    this.parent(el, options);
+  },
+
+
+  awake: function()
+  {
+    this.mapOutletsToThis();
+  }
+});
