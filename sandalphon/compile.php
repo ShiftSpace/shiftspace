1c9e25e44b47d900ad0c6d7db730a40c1b33842d
dnolen <david.nolen@gmail.com>
2009-09-22 04:48:57 -0400

    regular .php html files -> .html. removed old php code that called into python scripts.

--- sandalphon/compile.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo shell_exec("python sandalphon.py -i" . $_POST['filepath'] . " -o ../client/compiledViews/");
-?>
--- sandalphon/compile_and_load.php
+++
@@ -1,4 +0,0 @@
-<?php
-$command = "python ../sandalphon/sandalphon.py -i " . $_GET['filepath'] . " -j";
-echo shell_exec($command);
-?>
