a52e967ea8767e3e3fb7da09f6b396d043d365a4
dnolen <david.nolen@gmail.com>
2009-09-28 17:42:00 -0400

    * spaces/HelloWorld/HelloWorld.js: match HelloWorld tutorial.

--- spaces/HelloWorld/HelloWorld.js
+++ spaces/HelloWorld/HelloWorld.js
@@ -7,15 +7,12 @@ var HelloWorldShift = new Class({
   Extends: ShiftSpace.Shift,
   
   setup: function(json) {
-    // set the position
-    this.element.setStyles({
-      left: json.position.x,
-      top: json.position.y
-    });
-    // add your custom events
+    this.messageValue = json.message || "Hello World!";
+    this.element.setStyles({left: json.position.x, top: json.position.y});
+    this.element.set('text', this.messageValue);
     this.addEvents();
-    // add your interface to the page
     $(document.body).grab(this.element);
+    this.save();
   },
   
   addEvents: function(){
@@ -23,6 +20,23 @@ var HelloWorldShift = new Class({
       handle: this.element,
       onComplete: this.save.bind(this)
     });
+    this.element.addEvent('dblclick', this.changeMessage.bind(this));
+  },
+
+  changeMessage: function() {
+    var msg = prompt("Please enter a new message:", this.messageValue);
+    this.messageValue = msg;
+    this.element.set('html', msg);
+    this.save();
+  },
+
+  encode: function() {
+    var pos = this.element.getPosition();
+    return {
+      summary: this.messageValue,
+      message: this.messageValue,
+      position: pos
+    };
   }
 });
 
