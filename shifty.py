018655022193a864a73844aa8fbd59d47381766f
dnolen <david.nolen@gmail.com>
2009-09-29 19:08:09 -0400

    * docs/hello.txt: added smallest hello world space text. added shifty.py.

---
+++ shifty.py
@@ -0,0 +1,7 @@
+# The Happy Shifty Tool
+# In the future this tool will manage all of your ShiftSpace fun
+#   python shifty.py initdb
+#   python shifty.py installdeps
+#   python shifty.py setup
+#   python shifty.py updatedb
+#   python shifty.py new MyNewSpace
