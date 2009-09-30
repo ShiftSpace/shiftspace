ed0549e8c28bdf4fc11787fad4c130f923716851
dnolen <david.nolen@gmail.com>
2009-09-30 11:34:42 -0400

    * shifty.py: help for shifty.

--- shifty.py
+++ shifty.py
@@ -1,7 +1,42 @@
-# The Happy Shifty Tool
-# In the future this tool will manage all of your ShiftSpace fun
-#   python shifty.py initdb
-#   python shifty.py installdeps
-#   python shifty.py setup
-#   python shifty.py updatedb
-#   python shifty.py new MyNewSpace
+import sys
+import os
+
+def main(argv):
+    try:
+        action = argv[0]
+    except Error:
+        usage()
+        sys.exit(2)
+    if action in ("-h", "--help"):
+        usage()
+    if action == "installdeps":
+        print "installdeps"
+    elif action == "compile":
+        print "compile"
+    elif action == "initdb":
+        print "initdb"
+    elif action == "updatedb":
+        print "updatedb"
+    elif action == "new":
+        print "new"
+    elif action == "server":
+        print "server"
+
+
+def usage():
+    print
+    print "Hello from Shifty! <item> is required, [item] is not."
+    print "   %15s  install dependencies" % "installdeps"
+    print "   %15s  update ShiftSpace source" % "compile"
+    print "   %15s  initialize the database" % "initdb"
+    print "   %15s  update the database" % "updatedb"
+    print "   %15s  create a new space" % "new <SpaceName>"
+    print "   %15s  start ShiftServer on the specified port" % "server [port]"
+    print
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        main(sys.argv[1:])
+    else:
+        usage()
