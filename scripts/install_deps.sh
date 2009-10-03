bdb0a42d664f9646aaa69cd81371a287e8eaa247
dnolen <david.nolen@gmail.com>
2009-10-03 16:57:21 -0400

    * scripts/install_deps.sh: use the tmp directory for deps unpacking.

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,3 +1,4 @@
+cd tmp
 curl -O http://cloud.github.com/downloads/ShiftSpace/shiftspace/deps.tar.gz
 tar xvzf deps.tar.gz
 cd deps/CherryPy-3.1.2
@@ -11,7 +12,7 @@ sudo python setup.py install
 cd ../CouchDB-0.6
 sudo python setup.py install
 cd ../../
-mv deps/couchdb-lucene-0.4-jar-with-dependencies.jar server/
-mv deps/NaturalDocs-1.4 externals/
+mv deps/couchdb-lucene-0.4-jar-with-dependencies.jar ../server/
+mv deps/NaturalDocs-1.4 ../externals/
 rm -rf deps
 rm deps.tar.gz
