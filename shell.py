1d118f385820e5ff0b5bef4100da179be628fbf6
dnolen <david.nolen@gmail.com>
2009-11-13 18:51:51 -0500

    added shift to shell

--- shell.py
+++ shell.py
@@ -1,5 +1,6 @@
 from server.tests.dummy_data import *
 from server.models.ssuserschema import *
+from server.models.shiftschema import *
 from server.models.groupschema import *
 from server.models.permschema import *
 db = core.connect()
