985778785b9c6bd4d432a8c8e5831677285e6578
dnolen <david.nolen@gmail.com>
2009-11-19 19:08:26 -0500

    * shell.py: fixes for shell.py

--- shell.py
+++ shell.py
@@ -1,8 +1,9 @@
+import server.models.core as core
 from server.tests.dummy_data import *
-from server.models.ssuser import *
-from server.models.shift import *
-from server.models.group import Group import *
-from server.models.permission import *
-from server.models.favorite import *
-from server.models.comment import Comment import *
+from server.models.ssuser import SSUser
+from server.models.shift import Shift
+from server.models.group import Group
+from server.models.permission import Permission
+from server.models.favorite import Favorite
+from server.models.comment import Comment
 db = core.connect()
