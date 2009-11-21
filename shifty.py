c1963160b95911f83818cc41979c7d4d95af9bd7
dnolen <david.nolen@gmail.com>
2009-11-21 15:00:30 -0500

    cleaner handling of actually errors vs imports

--- shifty.py
+++ shifty.py
@@ -8,27 +8,25 @@ missing = []
     
 try:
     import simplejson as json
+except Exception as err:
+    missing.append("simplejson")
+try:
     import builder.preprocess as preprocess
     import builder.corebuilder as corebuilder
     import sandalphon.sandalphon as sandalphon
-except Exception as err:
+except Exceptions as err:
     print err
-    missing.append("simplejson")
-
 try:
     import manual.build as manbuild
 except Exception as err:
-    print err
     missing.append("mako")
 try:
     import server.setup as setup
 except Exception as err:
-    print err
     missing.append("couchdb-python")
 try:
     import server.server as server
 except Exception as err:
-    print err
     missing.append("cherrypy")
 
 
