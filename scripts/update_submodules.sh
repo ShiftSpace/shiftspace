e3104e233a1252b6b16c1b10188d8afb09059fb5
dnolen <david.nolen@gmail.com>
2009-11-21 17:30:44 -0500

    Ticket #205: Make it so that shifty.py can update git submodules

---
+++ scripts/update_submodules.sh
@@ -0,0 +1,32 @@
+cd externals/functools
+git branch --track shiftspace origin/shiftspace
+git checkout shiftspace
+git pull origin shiftspace
+cd ../fxgraph
+git branch --track shiftspace origin/shiftspace
+git checkout shiftspace
+git pull origin shiftspace
+cd ../gmmods
+git branch --track shiftspace origin/shiftspace
+git checkout shiftspace
+git pull origin shiftspace
+cd ../gmrequest
+git branch --track shiftspace origin/shiftspace
+git checkout shiftspace
+git pull origin shiftspace
+cd ../promises
+git branch --track shiftspace origin/shiftspace
+git checkout shiftspace
+git pull origin shiftspace
+cd ../rounded-image
+git branch --track shiftspace origin/shiftspace
+git checkout shiftspace
+git pull origin shiftspace
+cd ../set
+git branch --track shiftspace origin/shiftspace
+git checkout shiftspace
+git pull origin shiftspace
+cd ../ssunit
+git branch --track shiftspace origin/shiftspace
+git checkout shiftspace
+git pull origin shiftspace
