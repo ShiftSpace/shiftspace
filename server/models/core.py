236c00f37a09361e2a0b839670bc8b8269c48529
dnolen <david.nolen@gmail.com>
2009-11-26 00:39:56 -0600

    * server/models/core.py: fix for shifts not getting indexed by lucene if missing summary.

--- server/models/core.py
+++ server/models/core.py
@@ -34,7 +34,7 @@ class Lucene():
                     if(doc.type == 'shift') {                                 \
                       var ret = new Document();                               \
                       ret.add(doc.createdBy, {field:'createdBy'});            \
-                      ret.add(doc.summary, {field:'summary'});                \
+                      if (doc.summary) ret.add(doc.summary, {field:'summary'}); \
                       ret.add(doc.href, {field:'href'});                      \
                       ret.add(doc.domain, {field:'domain'});                  \
                       ret.add(doc.publishData.dbs.join(' '), {field:'dbs'});  \
