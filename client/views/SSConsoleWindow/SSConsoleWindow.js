53c56ead351105f4c078a3dc58c0d30017d52992
dnolen <david.nolen@gmail.com>
2009-12-02 20:26:43 -0500

    * client/views/SSConsoleWindow/SSConsoleWindow.css: added SSConsoleWindow.

---
+++ client/views/SSConsoleWindow/SSConsoleWindow.js
@@ -0,0 +1,65 @@
+// ==Builder==
+// @uiclass
+// @package           ShiftSpaceUI
+// @dependencies      SSFramedView
+// ==/Builder==
+
+/*
+  Class: SSConsoleWindow
+    The Console Window class. Comments, the Publish Pane, User details, Group details,
+    Space settings, they all live here :)
+*/
+var SSConsoleWindow = new Class({
+
+  Extends: SSFramedView,
+  name: "SSConsoleWindow",
+
+  defaults: function() {
+    return $merge(this.parent(), {
+      multipleSelection: true
+    });
+  },
+  
+  
+  initialize: function(el, options)
+  {
+    this.parent(el, options);
+  },
+  
+
+  attachEvents: function()
+  {
+  },
+  
+  
+  /* SSFramedView Stuff ============================ */
+  
+  awake: function() {},
+  
+  
+  onInterfaceLoad: function(ui)
+  {
+    this.parent(ui);
+    // TODO: Not super intuitive need someway to specify this automatically - David
+    this.element.setProperty('id', 'SSConsoleWindow');
+    this.element.addClass("SSDisplayNone");
+  }.asPromise(),
+  
+  
+  onContextActivate: function(context)
+  {
+    if(context == this.element.contentWindow)
+    {
+      this.mapOutletsToThis();
+      this.attachEvents();
+    }
+  },
+  
+  
+  buildInterface: function()
+  {
+    this.parent();
+    SSPostNotification('onConsoleWindowLoad', this);
+    this.setIsLoaded(true);
+  }
+});
