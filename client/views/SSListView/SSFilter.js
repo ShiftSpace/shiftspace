8bdfc03ff12ad75046207acd77c94ed5d68f9c67
dnolen <david.nolen@gmail.com>
2009-12-09 19:10:12 -0500

    * client/views/SSListView/SSFilter.js: added SSFilter.

---
+++ client/views/SSListView/SSFilter.js
@@ -0,0 +1,12 @@
+// ==Builder==
+// @uiclass
+// @package           ShiftSpaceCoreUI
+// @dependencies      SSListView
+// ==/Builder==
+
+var SSFilter = new Class({
+  initialize: function(listView)
+  {
+    
+  }
+});
