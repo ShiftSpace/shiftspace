f8f67e06c87eb55a5b782a5d66a8d872ea5d8674
dnolen <david.nolen@gmail.com>
2009-12-11 20:22:06 -0500

    * client/views/GroupsPane/GroupsPane.css: styling for people pane. show follower/following count on tab.

--- client/views/PeoplePane/PeoplePane.js
+++ client/views/PeoplePane/PeoplePane.js
@@ -30,6 +30,39 @@ var PeoplePane = new Class({
   },
 
 
+  show: function()
+  {
+    this.parent();
+    this.showCounts(SSUserInfo(ShiftSpaceUser.getUserName()));
+  },
+
+
+  showCounts: function(userData)
+  {
+    var followingCount = userData.followingCount,
+        followerCount = userData.followerCount,
+        controlView = this.PeopleTabView.controlView(),
+        followingEl = controlView.getElement(".followingCount"),
+        followerEl = controlView.getElement(".followerCount");
+    if(followingCount)
+    {
+      followingEl.set("text", "("+followingCount+")");
+    }
+    else
+    {
+      followingEl.set("text", "");
+    }
+    if(followerCount)
+    {
+      followerEl.set("text", "("+followerCount+")");
+    }
+    else
+    {
+      followerEl.set("text", "");
+    }
+  }.asPromise(),
+
+
   hide: function()
   {
     this.parent();
