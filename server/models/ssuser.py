0e5c389b8cbfffd8ee4344b6d65474055ac9ae76
dnolen <david.nolen@gmail.com>
2009-12-17 01:15:14 -0500

    * server/models/ssuser.py: fixed bug in SSUser.unreadCount

--- server/models/ssuser.py
+++ server/models/ssuser.py
@@ -430,9 +430,8 @@ class SSUser(User):
     def unreadCount(self):
         from server.models.message import Message
         db = core.connect("shiftspace/shared")
-        syscount = core.value(Message.system_count(db)) or 0
-        tocount = core.value(Message.count_by_user(db)) or 0
-        readcount = core.value(Message.read_count_by_user(db)) or 0
-        print "syscount %s, tocount %s, readcount %s" % (syscount, tocount, readcount)
+        syscount = core.value(Message.system_count(db, key=self.id)) or 0
+        tocount = core.value(Message.count_by_user(db, key=self.id)) or 0
+        readcount = core.value(Message.read_count_by_user(db, key=self.id)) or 0
         return (syscount+tocount)-readcount
 
