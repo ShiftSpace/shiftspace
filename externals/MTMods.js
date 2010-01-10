ed1df09bb823b8707dc7bde888bf652a8ae444df
dnolen <david.nolen@gmail.com>
2010-01-09 19:21:32 -0500

    * externals/MTMods.js: added DelayedAsset inorder to use with promises.

--- externals/MTMods.js
+++ externals/MTMods.js
@@ -20,6 +20,7 @@ var _urlJoin = Function.dispatch(
 
 
 String.implement({
+
   pop: function() { return this.substring(1, this.length); },
 
   tail: function(n) { return this.substring(this.length-(n || 1)); },
@@ -466,3 +467,46 @@ function SSTemplate(el, props)
     if(target) target.set(attr, value);
   }, this);
 }
+
+
+var DelayedAsset = new Class({
+  Implements: Events,
+  
+  initialize: function(type, source, properties) {
+    this.type = type;
+    this.source = source;
+    this.properties = properties;
+  },
+
+  load: function() {
+    switch(this.type) {
+      case 'javascript':
+        this.asset = new Asset.javascript(this.source, $merge(this.properties, {
+          onload: function() {
+            if($callable(this.properties.onload)) this.properties.onload(this.asset);
+            this.fireEvent('onload', this.asset);
+          }.bind(this)
+        }));
+        break;
+      case 'css':
+        this.asset = new Asset.css(this.source, $merge(this.properties));
+        break;
+      case 'image':
+        this.asset = new Asset.image(this.source, $merge(this.properties, {
+          onload: function() {
+            if($callable(this.properties.onload)) this.properties.onload(this.asset);
+            this.fireEvent('onload', this.asset);
+          }.bind(this),
+          onabort: function() {
+            if($callable(this.properties.onabort)) this.properties.onabort(this.asset);
+            this.fireEvent('onabort', this.asset);
+          }.bind(this),
+          onerror: function() {
+            if($callable(this.properties.onerror)) this.properties.onerror(this.asset);
+            this.fireEvent('onerror', this.asset);
+          }.bind(this)
+        }));
+        break;
+    }
+  }
+});
