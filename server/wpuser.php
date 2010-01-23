e5f7427560f548050066e4aaec813d66b19967a7
dnolen <david.nolen@gmail.com>
2010-01-23 16:04:06 -0500

    * server/wpuser.php: fix query to return JSON object.

--- server/wpuser.php
+++ server/wpuser.php
@@ -4,27 +4,27 @@ require_once('user.php');
 
 class WPUserController extends UserController {
   public function __construct() {   
-	global $current_user;
-	$userData;
+    global $current_user;
+    $userData;
     if($this->isLoggedIn()) {
-    	$this->userData = array(
-	    					"userName" => $current_user->display_name,
-	    					"_id" => $current_user->ID,
-	    					"userLevel" => $current_user->user_level
-	    					);
-	   	$this->userData = json_encode($this->userData);
+      $this->userData = array(
+                              "userName" => $current_user->display_name,
+                              "_id" => $current_user->ID,
+                              "userLevel" => $current_user->user_level
+                              );
+      $this->userData = json_encode($this->userData);
     } else {
-    	$this->userData = "not logged in.";
+      $this->userData = "{message:'not logged in.'}";
     
     }
   }
   
   public function matchUser() {
-  	return $this->userData;
+    return $this->userData;
   }
   
   public function isLoggedIn() {
-  	return is_user_logged_in();
+    return is_user_logged_in();
   }
 }
 ?>
