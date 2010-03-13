77a81a6421211b287e83305b2f5534c1fa75d6ee
dnolen <david.nolen@gmail.com>
2010-03-13 17:04:57 -0500

    * server/controllers/shift.py: change shifts signatures to take all and bySpace parameters for autolaunch support.

--- server/models/shift.py
+++ server/models/shift.py
@@ -521,7 +521,7 @@ class Shift(SSDocument):
     # ========================================
 
     @classmethod
-    def shifts(cls, user, byHref=None, byDomain=None, byFollowing=False, byGroups=False, start=0, limit=25, filter=False, query=None):
+    def shifts(cls, user, byHref=None, byDomain=None, byFollowing=False, byGroups=False, bySpace=None, start=0, limit=25, filter=False, query=None, all=False):
         from server.models.ssuser import SSUser
         db = core.connect("shiftspace/shared")
         lucene = core.lucene()
