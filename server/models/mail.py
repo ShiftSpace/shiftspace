75797ef84e4b8d9f90bb5d5f092b3b7933a5b8e3
David Nolen <davidnolen@melville.westell.com>
2010-05-01 17:13:54 -0400

    mail wip, getting close to building password retrieval.

--- server/models/mail.py
+++ server/models/mail.py
@@ -6,3 +6,9 @@ import server.server as server
 
 parser = ConfigParser.ConfigParser()
 options = parser.readfp(open(os.path.join(server.SERVER_ROOT, "mail.conf")))
+
+def sendMail(to=None, text=None):
+    server = smtplib.SMTP(smtpServer)
+    server.set_debuglevel(1)
+    server.sendmail(options.get("Mail Settings", "from"), to, text)
+    server.quit()
